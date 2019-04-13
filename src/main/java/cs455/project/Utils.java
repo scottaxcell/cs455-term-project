b293dc194857bbea271c3b965611701a2df2f873
coherentchaos <scott.axcell@gmail.com>
2019-04-13 15:25:08 -0600

    update README. add spark bin

---
+++ src/main/java/cs455/project/Utils.java
@@ -0,0 +1,30 @@
+package cs455.project;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Utils {
+    private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("HH:mm:ss");
+    private static boolean DEBUG = true;
+
+    public static void out(Object o) {
+        System.out.print(o);
+    }
+
+    public static void info(Object o) {
+        System.out.println("\nINFO: " + o);
+    }
+
+    public static void debug(Object o) {
+        if (DEBUG)
+            System.out.println(String.format("DEBUG: [%s] %s", SIMPLE_DATE_FORMAT.format(new Date()), o));
+    }
+
+    public static void error(Object o) {
+        System.err.println("\nERROR: " + o);
+    }
+
+    public static boolean isValidString(String str) {
+        return str != null && !str.isEmpty() && !str.trim().isEmpty();
+    }
+}
