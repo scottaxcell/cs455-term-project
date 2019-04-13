57b4179d5e17835267974b889b15ed116f57f788
coherentchaos <scott.axcell@gmail.com>
2019-04-13 13:02:34 -0600

    build.gradle, idea project, and start of word count example

---
+++ src/main/java/cs455/project/WordCount.java
@@ -0,0 +1,30 @@
+package cs455.project;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+
+public class WordCount {
+    public static void main(String[] args){
+
+        //Create a SparkContext to initialize
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("Word Count");
+
+        // Create a Java version of the Spark Context
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        // Load the text into a Spark RDD, which is a distributed representation of each line of text
+        JavaRDD<String> textFile = sc.textFile("src/main/resources/MobyDick.txt");
+        JavaPairRDD<String, Integer> counts = textFile
+                .flatMap(s -> Arrays.asList(s.split("[ ,]")).iterator())
+                .mapToPair(word -> new Tuple2<>(word, 1))
+                .reduceByKey((a, b) -> a + b);
+        counts.foreach(p -> System.out.println(p));
+        System.out.println("Total words: " + counts.count());
+        counts.saveAsTextFile("src/main/resources/shakespeareWordCount");
+    }
+}
