1995cdb2cd7e16a6847cb3441aa3fccf06d9adaa
Scott Axcell <scott.axcell@gmail.com>
2019-04-15 09:39:44 -0600

    location description set

--- runme.sh
+++ runme.sh
@@ -8,3 +8,11 @@ $SPARK_HOME/bin/spark-submit \
 --deploy-mode cluster \
 --class cs455.project.PrimaryTypeSet \
 --supervise build/libs/cs455TermProject.jar
+
+$HADOOP_HOME/bin/hdfs dfs -rm -r /user/${USER}/locationDescriptionSet
+gradle build && \
+$SPARK_HOME/bin/spark-submit \
+--master spark://phoenix:30318 \
+--deploy-mode cluster \
+--class cs455.project.LocationDescriptionSet \
+--supervise build/libs/cs455TermProject.jar
--- src/main/java/cs455/project/LocationDescriptionSet.java
+++ src/main/java/cs455/project/LocationDescriptionSet.java
@@ -7,27 +7,27 @@ import org.apache.spark.api.java.JavaSparkContext;
 import java.util.List;
 
 
-public class PrimaryTypeSet {
+public class LocationDescriptionSet {
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setAppName("Primary Type Set");
+        SparkConf conf = new SparkConf().setAppName("Location Description Set");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> textFile = sc.textFile(Constants.HDFS_CRIMES_DIR);
-        List<String> uniquePrimaryTypes = textFile
-                .map(PrimaryTypeSet::getPrimaryType)
+        List<String> uniqueLocationDescriptions = textFile
+                .map(LocationDescriptionSet::getLocationDescription)
                 .filter(Utils::isValidString)
                 .distinct()
                 .sortBy(s -> s, true, 1)
                 .collect();
 
-        sc.parallelize(uniquePrimaryTypes, 1)
-                .saveAsTextFile("primaryTypeSet");
+        sc.parallelize(uniqueLocationDescriptions, 1)
+                .saveAsTextFile("locationDescriptionSet");
     }
 
-    private static String getPrimaryType(String s) {
+    private static String getLocationDescription(String s) {
         String[] split = Utils.splitCommaDelimitedString(s);
         if (split.length != CrimesIndices.NUM_FIELDS)
             return "";
-        return split[CrimesIndices.PRIMARY_TYPE_INDEX];
+        return split[CrimesIndices.LOCATION_DESCRIPTION_INDEX];
     }
 }
