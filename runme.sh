0e32771037a657c790f1c5c2aa5133236f288ca8
coherentchaos <scott.axcell@gmail.com>
2019-04-23 20:29:20 -0600

    remove prototypes from runme. indoor vs outdoor (in-progress)

--- runme.sh
+++ runme.sh
@@ -2,34 +2,6 @@
 
 PERSONAL_SPARK=phoenix:30318
 
-$HADOOP_HOME/bin/hdfs dfs -rm -r /user/${USER}/primaryTypeSet
-$SPARK_HOME/bin/spark-submit \
---master spark://$PERSONAL_SPARK \
---deploy-mode cluster \
---class cs455.project.prototypes.PrimaryTypeSet \
---supervise build/libs/cs455TermProject.jar
-
-$HADOOP_HOME/bin/hdfs dfs -rm -r /user/${USER}/locationDescriptionSet
-$SPARK_HOME/bin/spark-submit \
---master spark://$PERSONAL_SPARK \
---deploy-mode cluster \
---class cs455.project.prototypes.LocationDescriptionSet \
---supervise build/libs/cs455TermProject.jar
-
-$HADOOP_HOME/bin/hdfs dfs -rm -r /user/${USER}/primaryTypeBreakdown
-$SPARK_HOME/bin/spark-submit \
---master spark://$PERSONAL_SPARK \
---deploy-mode cluster \
---class cs455.project.prototypes.PrimaryTypeBreakdown \
---supervise build/libs/cs455TermProject.jar
-
-$HADOOP_HOME/bin/hdfs dfs -rm -r /user/${USER}/daysOfFullMoon
-$SPARK_HOME/bin/spark-submit \
---master spark://$PERSONAL_SPARK \
---deploy-mode cluster \
---class cs455.project.prototypes.DaysOfFullMoon \
---supervise build/libs/cs455TermProject.jar
-
 $HADOOP_HOME/bin/hdfs dfs -rm -r /user/${USER}/FullMoonCrimeStats
 $SPARK_HOME/bin/spark-submit \
 --master spark://$PERSONAL_SPARK \
