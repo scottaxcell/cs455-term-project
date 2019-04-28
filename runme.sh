0580ac88dba8e1ca26b639cad084c7af592f699d
Matt Muller <justmatt@colostate.edu>
2019-04-28 10:46:32 -0500

    Add weekly data to runme.sh

--- runme.sh
+++ runme.sh
@@ -2,6 +2,27 @@
 
 PERSONAL_SPARK=phoenix:30318
 
+$HADOOP_HOME/bin/hdfs dfs -rm -r /user/${USER}/CrimeStats
+$SPARK_HOME/bin/spark-submit \
+--master spark://$PERSONAL_SPARK \
+--deploy-mode cluster \
+--class cs455.project.drivers.CrimeStats \
+--supervise build/libs/cs455TermProject.jar
+
+HADOOP_HOME/bin/hdfs dfs -rm -r /user/${USER}/DayOfWeekCrimeStats
+$SPARK_HOME/bin/spark-submit \
+--master spark://$PERSONAL_SPARK \
+--deploy-mode cluster \
+--class cs455.project.drivers.DayOfWeekCrimeStats \
+--supervise build/libs/cs455TermProject.jar
+
+HADOOP_HOME/bin/hdfs dfs -rm -r /user/${USER}/DayOfWeekCrimeStatsWithFullMoon
+$SPARK_HOME/bin/spark-submit \
+--master spark://$PERSONAL_SPARK \
+--deploy-mode cluster \
+--class cs455.project.drivers.DayOfWeekCrimeStatsWithFullMoon \
+--supervise build/libs/cs455TermProject.jar
+
 $HADOOP_HOME/bin/hdfs dfs -rm -r /user/${USER}/FullMoonCrimeStats
 $SPARK_HOME/bin/spark-submit \
 --master spark://$PERSONAL_SPARK \
